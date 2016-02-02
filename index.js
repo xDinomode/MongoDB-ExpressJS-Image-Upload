3033b312bb1e84563c357e34f37e6de738bd2ae9
xDinomode <xDinomode@users.noreply.github.com>
2016-02-02 11:05:13 -0600

    improved

--- index.js
+++ index.js
@@ -21,36 +21,54 @@ Grid.mongo = mongoose.mongo;
 
 conn.once("open", function(){
   gfs = Grid(conn.db);
+  app.get("/", function(req,res){
+    //renders a multipart/form-data form
+    res.render("home");
+  });
+
+  //second parameter is multer middleware.
+  app.post("/", upload.single("avatar"), function(req, res, next){
+    //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
+    var writestream = gfs.createWriteStream({
+      filename: req.file.originalname
+    });
+    //
+    // //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
+    fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
+        .on("err", function(){res.send("Error uploading image")})
+          .pipe(writestream);
+  });
+
+  // sends the image we saved by filename.
+  app.get("/:filename", function(req, res){
+      var readstream = gfs.createReadStream({filename: req.params.filename});
+      readstream.on("error", function(err){
+        res.send("No image found with that title");
+      });
+      readstream.pipe(res);
+  });
+
+  //delete the image
+  app.get("/delete/:filename", function(req, res){
+    gfs.exist({filename: req.params.filename}, function(err, found){
+      if(err) return res.send("Error occured");
+      if(found){
+        gfs.remove({filename: req.params.filename}, function(err){
+          if(err) return res.send("Error occured");
+          res.send("Image deleted!");
+        });
+      } else{
+        res.send("No image found with that title");
+      }
+    });
+  });
 });
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get("/", function(req,res){
-  //renders a multipart/form-data form
-  res.render("home");
-});
 
-//second parameter is multer middleware.
-app.post("/", upload.single("avatar"), function(req, res, next){
-
-  //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
-  var writestream = gfs.createWriteStream({
-    filename: req.file.originalname,
-  });
-
-  //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
-  fs.createReadStream("./uploads/" + req.file.filename)
-    .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
-      .on("err", function(){res.send("error")})
-        .pipe(writestream);
-});
-
-//sends the image we saved by filename.
-app.get("/:filename", function(req, res){
-  gfs.createReadStream({filename: req.params.filename})
-    .pipe(res);
-});
 
 if (!module.parent) {
   app.listen(3000);
